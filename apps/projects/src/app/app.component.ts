1363b9a000bc90e57d5e40c213698b124c94ff62
Victor Avila <victor.avila@venturplex.com>
2020-02-12 09:49:16 -0700

    feat: build out toolbar

--- apps/projects/src/app/app.component.ts
+++ apps/projects/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projects';
+  links = [
+    {path: '/projects', label: 'PROJECTS', icon: 'loyalty'},
+    {path: '/login', label: 'LOGIN', icon: 'person'},
+  ];
 }
