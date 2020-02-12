aa140f391001c46ca5013387a890adbead5c6b75
Victor Avila <victor.avila@venturplex.com>
2020-02-12 09:01:12 -0700

    feat: add material and setup styling

--- apps/projects/src/app/app.module.ts
+++ apps/projects/src/app/app.module.ts
@@ -1,11 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+
+import { CoreDataModule } from '@workspace/core-data';
+import { CoreStateModule } from '@workspace/core-state';
+import { MaterialModule } from '@workspace/material';
+import { UiToolbarModule } from '@workspace/ui-toolbar';
+import { UiLoginModule } from '@workspace/ui-login';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
+    UiToolbarModule,
+    UiLoginModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
